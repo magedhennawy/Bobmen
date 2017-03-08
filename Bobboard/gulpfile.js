57ddcabf03241a0defb2005826c0c852c1b8b4e4
Brandon <brandon.bowen@mail.utoronto.ca>
2017-03-08 14:46:09 -0500

    added main-bower-files

--- Bobboard/gulpfile.js
+++ Bobboard/gulpfile.js
@@ -1,9 +1,10 @@
+var bowerFiles = require('main-bower-files');
 var gulp = require('gulp');
 var inject = require('gulp-inject');
 
 gulp.task('default', function() {
   // place code for your default task here
   var target = gulp.src('./public/views/index.html');
-  var sources = gulp.src(['./bower_components/angular/angular.min.js'], {read: false}, {relative: true});
+  var sources = gulp.src(bowerFiles(), {read: false}, {name:'bower'});
   return target.pipe(inject(sources)).pipe(gulp.dest('./public/views/'));
 });
