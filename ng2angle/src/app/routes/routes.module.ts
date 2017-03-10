4bd71ff378e00b6751161bed1baede9c28f6c4e8
Maged <magedhennawy@gmail.com>
2017-03-10 15:43:24 -0500

    empty dashboard, lets start with login screen (assigned to brandon)

--- ng2angle/src/app/routes/routes.module.ts
+++ ng2angle/src/app/routes/routes.module.ts
@@ -3,7 +3,6 @@ import { RouterModule } from '@angular/router';
 
 import { MenuService } from '../core/menu/menu.service';
 import { SharedModule } from '../shared/shared.module';
-import { PagesModule } from './pages/pages.module';
 
 import { menu } from './menu';
 import { routes } from './routes';
@@ -11,8 +10,7 @@ import { routes } from './routes';
 @NgModule({
     imports: [
         SharedModule,
-        RouterModule.forRoot(routes),
-        PagesModule
+        RouterModule.forRoot(routes)
     ],
     declarations: [],
     exports: [
