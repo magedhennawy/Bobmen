1aded44eddd22e984f5886eef7882f888f64c01f
Maged <magedhennawy@gmail.com>
2017-03-25 17:11:29 -0400

    google stuff set up

--- ng2angle/src/app/routes/widgets/google/google.component.ts
+++ ng2angle/src/app/routes/widgets/google/google.component.ts
@@ -1,13 +1,37 @@
 import { Component,OnInit } from '@angular/core';
+import { GoogleService } from '../../../shared/services/google.service';
+import {Observable} from 'rxjs/Rx';
 
 
 @Component({
   selector: 'app-google',
-  templateUrl: 'google.component.html'
+  templateUrl: './google.component.html',
+  styleUrls: ['./google.component.scss'],
+  providers: [GoogleService],
 })
 export class GoogleComponent implements OnInit  {
 
-  ngOnInit(){
+  constructor(private google: GoogleService){ }
 
+  result = [];
+  getEmails = function(){
+    this.google.getEmails().catch(err =>  {
+      document.getElementById("googleConnect").style.display = "block";
+    }).subscribe(
+      data => {
+        document.getElementById("googleConnect").style.display = "none";
+        this.result = data.threads;
+      })
+  };
+  ngOnInit(){
+    this.getEmails();
+  }
+
+  getHomeTimeline() {
+    this.getEmails();
+  }
+
+  connectToGoogle(){
+    location.href = "/api/auth/google";
   }
 }
---
+++ ng2angle/src/app/shared/services/google.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
+
+@Injectable()
+export class GoogleService {
+
+  constructor(private http: Http) { }
+
+  // Get all posts from the API
+  getEmails() {
+    return this.http.get('/api/google/emails')
+      .catch(err =>  {
+        return Observable.throw(err.status); // observable needs to be returned or exception raised
+      })
+      .map((res:Response) => res.json());
+  }
+
+  /*addItem(item) {
+   return this.http.post('/api/todolist',item)
+   .map((res:Response) => res.json());
+   }
+
+   editItem(item) {
+   return this.http.patch('/api/todolist',item)
+   .map((res:Response) => res.json());
+   }
+
+   deleteItem(index) {
+   return this.http.delete('/api/todolist/'+index)
+   .map((res:Response) => res.json());
+   }*/
+}
