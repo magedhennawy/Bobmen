34229335502c986d46140395635ad101f0ae166e
Paul Bursu <paulbursu@gmail.com>
2017-03-28 23:14:08 -0400

    PLS DOCKER

--- ng2angle/server.js
+++ ng2angle/server.js
@@ -108,7 +108,7 @@ io.on('connection', function (socket) {
  * Create HTTP server.
  */
 server.listen(3000, function () {
-  console.log('HTTP on port 3000');
+  console.log('HTTP  on port 3000');
 });//redeploy
 
 /**
